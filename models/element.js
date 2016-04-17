d5aea2956a582c7aab44e6d13aa8d1e642683739
Steve Cochrane <steve@stevecochrane.com>
2016-04-17 17:44:39 -0500

    Fussy formatting edit for personal preference.

--- models/element.js
+++ models/element.js
@@ -1,8 +1,8 @@
 var mongoose = require("mongoose");
 
 var elementSchema = mongoose.Schema({
-    name: String,
-    description: String
+    "name": String,
+    "description": String
 });
 
 var Element = mongoose.model("Element", elementSchema);
