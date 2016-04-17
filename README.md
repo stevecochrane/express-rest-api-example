a07743ecdecef4fe4ed10898c0ba7ec28db86517
Steve Cochrane <steve@stevecochrane.com>
2016-04-17 15:26:02 -0500

    First commit, no functionality yet.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Express REST API Example
+
+An example of a REST API built with Express. More info coming soon.
+
+A lot of this was learned from the excellent book
+[Web Development with Node & Express](http://shop.oreilly.com/product/0636920032977.do) by Ethan Brown.
---
+++ express-rest-api-example.js
@@ -0,0 +1,43 @@
+var compression    = require("compression");
+var express        = require("express");
+var slashes        = require("connect-slashes");
+var uncapitalize   = require("express-uncapitalize");
+
+var app = express();
+
+//  Treat "/foo" and "/Foo" as different URLs
+app.set("case sensitive routing", true);
+
+//  Treat "/foo" and "/foo/" as different URLs
+app.set("strict routing", true);
+
+//  Default to port 3000
+app.set("port", process.env.PORT || 3000);
+
+//  Compress all requests
+app.use(compression());
+
+//  Enforce trailing slashes for all URLs
+app.use(slashes());
+
+//  Enforce lowercase for all URLs
+app.use(uncapitalize());
+
+//  Custom 404 page
+app.use(function(req, res) {
+    res.type("text/plain");
+    res.status(404);
+    res.send("404 - Not Found");
+});
+
+//  Custom 500 page
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.type("text/plain");
+    res.status(500);
+    res.send("500 - Server Error");
+});
+
+app.listen(app.get("port"), function() {
+    console.log("Express started on http://localhost:" + app.get("port") + "; press Ctrl-C to terminate.");
+});
