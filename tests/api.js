c4c97d411f44e25e8609f8a2a1bb10adde4d2784
Steve Cochrane <steve@stevecochrane.com>
2016-06-11 23:33:04 -0500

    Reworded some conditions.

--- tests/api.js
+++ tests/api.js
@@ -4,7 +4,7 @@ var restler = require("restler");
 
 describe("Element API", function() {
 
-    var element = {
+    var testElement = {
         "name": "test",
         "description": "This is a element added by the API Test suite."
     };
@@ -12,21 +12,21 @@ describe("Element API", function() {
     var baseUrl = "http://localhost:3000";
 
     describe("Create", function() {
-        it("Should be able to add a new element", function(done) {
-            restler.post(baseUrl + "/api/elements", { "data": element }).on("success", function(data) {
-                assert.isDefined(data.id, "data.id is defined");
+        it("Should be able to create a new element", function(done) {
+            restler.post(baseUrl + "/api/elements", { "data": testElement }).on("success", function(data) {
+                assert.isDefined(data.element._id, "The new element's ID is defined");
                 done();
             });
         });
     });
 
     describe("Read", function() {
-        it("Should be able to retrieve an element", function(done) {
-            restler.post(baseUrl + "/api/elements", { "data": element }).on("success", function(data) {
-                assert.isDefined(data.id, "data.id is defined");
-                restler.get(baseUrl + "/api/element/" + data.id).on("success", function(data) {
-                    assert(data.name === element.name, "New element name is consistent with what was submitted");
-                    assert(data.description === element.description, "New element description is consistent with what was submitted");
+        it("Should be able to read an existing element", function(done) {
+            restler.post(baseUrl + "/api/elements", { "data": testElement }).on("success", function(data) {
+                assert.isDefined(data.element._id, "The new element's ID is defined");
+                restler.get(baseUrl + "/api/element/" + data.element._id).on("success", function(data) {
+                    assert(data.element.name === testElement.name, "New element name is consistent with what was submitted");
+                    assert(data.element.description === testElement.description, "New element description is consistent with what was submitted");
                     done();
                 });
             });
