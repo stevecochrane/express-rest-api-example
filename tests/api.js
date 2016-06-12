fe9dce1ea741cd3dcbf82dc2a7310dd9e7d45d93
Steve Cochrane <steve@stevecochrane.com>
2016-06-11 23:10:13 -0500

    Structured the tests after CRUD.

--- tests/api.js
+++ tests/api.js
@@ -2,7 +2,7 @@ var assert  = require("chai").assert;
 var http    = require("http");
 var restler = require("restler");
 
-describe("API Tests", function() {
+describe("Element API", function() {
 
     var element = {
         "name": "test",
@@ -11,22 +11,34 @@ describe("API Tests", function() {
 
     var baseUrl = "http://localhost:3000";
 
-    it("Should be able to add a new element", function(done) {
-        restler.post(baseUrl + "/api/elements", { "data": element }).on("success", function(data) {
-            assert.isDefined(data.id, "data.id is defined");
-            done();
-        });
-    });
-
-    it("Should be able to retrieve an element", function(done) {
-        restler.post(baseUrl + "/api/elements", { "data": element }).on("success", function(data) {
-            assert.isDefined(data.id, "data.id is defined");
-            restler.get(baseUrl + "/api/element/" + data.id).on("success", function(data) {
-                assert(data.name === element.name, "New element name is consistent with what was submitted");
-                assert(data.description === element.description, "New element description is consistent with what was submitted");
+    describe("Create", function() {
+        it("Should be able to add a new element", function(done) {
+            restler.post(baseUrl + "/api/elements", { "data": element }).on("success", function(data) {
+                assert.isDefined(data.id, "data.id is defined");
                 done();
             });
         });
+    });
+
+    describe("Read", function() {
+        it("Should be able to retrieve an element", function(done) {
+            restler.post(baseUrl + "/api/elements", { "data": element }).on("success", function(data) {
+                assert.isDefined(data.id, "data.id is defined");
+                restler.get(baseUrl + "/api/element/" + data.id).on("success", function(data) {
+                    assert(data.name === element.name, "New element name is consistent with what was submitted");
+                    assert(data.description === element.description, "New element description is consistent with what was submitted");
+                    done();
+                });
+            });
+        });
+    });
+
+    describe("Update", function() {
+        it("Should be able to update an existing element");
+    });
+
+    describe("Delete", function() {
+        it("Should be able to delete an element");
     });
 
 });
