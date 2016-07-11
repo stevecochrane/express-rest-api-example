a2889dfb0e9f4be393b0f4bb1e5e17dbdf97aba0
Steve Cochrane <steve@stevecochrane.com>
2016-07-10 21:52:09 -0500

    Separated out the controller logic for each route into a new controller.js file.

---
+++ controller.js
@@ -0,0 +1,74 @@
+//  Mongoose database schema
+var Element = require("./models/element.js");
+
+module.exports = {
+
+    viewAllElements: function(req, res) {
+        Element.find(function(err, elements) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(elements);
+        });
+    },
+
+    addElement: function(req, res) {
+        var element = new Element();
+        element.name = req.body.name;
+        element.description = req.body.description;
+
+        element.save(function(err) {
+            if (err) {
+                res.send(err);
+            }
+            res.json({
+                "message": "Element created!",
+                "element": element
+            });
+        });
+    },
+
+    viewElement: function(req, res) {
+        Element.findById(req.params.element_id, function(err, element) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(element);
+        });
+    },
+
+    updateElement: function(req, res) {
+        Element.findById(req.params.element_id, function(err, element) {
+            if (err) {
+                res.send(err);
+            }
+
+            element.name = req.body.name;
+            element.description = req.body.description;
+
+            element.save(function(err) {
+                if (err) {
+                    res.send(err);
+                }
+                res.json({
+                    "message": "Element updated!",
+                    "element": element
+                });
+            });
+        });
+    },
+
+    deleteElement: function(req, res) {
+        Element.remove({
+            "_id": req.params.element_id
+        }, function(err, element) {
+            if (err) {
+                res.send(err);
+            }
+            res.json({
+                "message": "Element deleted!"
+            });
+        });
+    }
+
+};
--- router.js
+++ router.js
@@ -1,86 +1,27 @@
 var express = require("express");
 var router  = express.Router();
 
-//  Mongoose database schema
-var Element = require("./models/element.js");
+var controller = require("./controller.js");
 
 //  For routes ending with "/elements"
 router.route("/elements")
 
     //  View all elements (accessed with GET at http://localhost:3000/api/elements)
-    .get(function(req, res) {
-        Element.find(function(err, elements) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(elements);
-        });
-    })
+    .get(controller.viewAllElements)
 
     //  Add a new element (accessed with POST at http://localhost:3000/api/elements)
-    .post(function(req, res) {
-        var element = new Element();
-        element.name = req.body.name;
-        element.description = req.body.description;
-
-        element.save(function(err) {
-            if (err) {
-                res.send(err);
-            }
-            res.json({
-                "message": "Element created!",
-                "element": element
-            });
-        });
-    });
+    .post(controller.addElement);
 
 //  For routes ending with "/elements/:element_id"
 router.route("/element/:element_id")
 
     //  View a specific element (accessed with GET at http://localhost:3000/api/elements/:element_id)
-    .get(function(req, res) {
-        Element.findById(req.params.element_id, function(err, element) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(element);
-        });
-    })
+    .get(controller.viewElement)
 
     //  Update a specific element (accessed with PUT at http://localhost:3000/api/elements/:element_id)
-    .put(function(req, res) {
-        Element.findById(req.params.element_id, function(err, element) {
-            if (err) {
-                res.send(err);
-            }
-
-            element.name = req.body.name;
-            element.description = req.body.description;
-
-            element.save(function(err) {
-                if (err) {
-                    res.send(err);
-                }
-                res.json({
-                    "message": "Element updated!",
-                    "element": element
-                });
-            });
-        });
-    })
+    .put(controller.updateElement)
 
     //  Delete a specific element (accessed with DELETE at http://localhost:3000/api/elements/:element_id)
-    .delete(function(req, res) {
-        Element.remove({
-            "_id": req.params.element_id
-        }, function(err, element) {
-            if (err) {
-                res.send(err);
-            }
-            res.json({
-                "message": "Element deleted!"
-            });
-        });
-    });
+    .delete(controller.deleteElement);
 
 module.exports = router;
