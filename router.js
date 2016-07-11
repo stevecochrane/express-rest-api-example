54c08e0aff9580329733de994c65169a921a76e4
Steve Cochrane <steve@stevecochrane.com>
2016-07-10 22:13:48 -0500

    Renamed controller.js to api.js.

--- router.js
+++ router.js
@@ -1,27 +1,27 @@
 var express = require("express");
 var router  = express.Router();
 
-var controller = require("./controller.js");
+var api = require("./api.js");
 
 //  For routes ending with "/elements"
 router.route("/elements")
 
     //  View all elements (accessed with GET at http://localhost:3000/api/elements)
-    .get(controller.viewAllElements)
+    .get(api.viewAllElements)
 
     //  Add a new element (accessed with POST at http://localhost:3000/api/elements)
-    .post(controller.addElement);
+    .post(api.addElement);
 
 //  For routes ending with "/elements/:element_id"
 router.route("/element/:element_id")
 
     //  View a specific element (accessed with GET at http://localhost:3000/api/elements/:element_id)
-    .get(controller.viewElement)
+    .get(api.viewElement)
 
     //  Update a specific element (accessed with PUT at http://localhost:3000/api/elements/:element_id)
-    .put(controller.updateElement)
+    .put(api.updateElement)
 
     //  Delete a specific element (accessed with DELETE at http://localhost:3000/api/elements/:element_id)
-    .delete(controller.deleteElement);
+    .delete(api.deleteElement);
 
 module.exports = router;
